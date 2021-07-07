d435efeed88ddc22b01e636b6c6bb14fc9ff672f
Andrew <fleming-andrew@protonmail.com>
2021-07-06 20:22:15 -0400

    Added nft factory with tests

--- test/NFTFactory.test.ts
+++ test/NFTFactory.test.ts
@@ -2,12 +2,6 @@ import { ethers, upgrades } from "hardhat";
 import { expect } from "chai";
 import { Contract } from "ethers"
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { toUtf8Bytes } from "ethers/lib/utils";
-
-const formatNftBalance = async(contract: Contract, user: string, id: number): Promise<number> => {
-    let balance = ethers.utils.formatEther(await contract.balanceOf(user, id));
-    return Number(balance) * 10**18;
-}
 
 describe("NFTFactory", () => {
 
@@ -19,14 +13,22 @@ describe("NFTFactory", () => {
     let owner: SignerWithAddress;
     let alice: SignerWithAddress;
 
+    const minter = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("MINTER_ROLE"));
+    const pauser = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("PAUSER_ROLE"));
+    const jackPrice = ethers.utils.parseEther("5");
+    const sallyPrice = ethers.utils.parseEther("5");
+
+
     beforeEach(async() => {
         const NFTFactory = await ethers.getContractFactory("NFTFactory");
         const NFTFactoryV2 = await ethers.getContractFactory("NFTFactoryV2");
         [owner, alice] = await ethers.getSigners();
-        jack = await upgrades.deployProxy(NFTFactory, ["jack.token"]);
+        jack = await upgrades.deployProxy(NFTFactory, ["Jack-O-Lantern", "JACK", "jack.token", jackPrice]);
         jackV2 = await upgrades.upgradeProxy(jack.address, NFTFactoryV2);
-        sally = await upgrades.deployProxy(NFTFactory, ["sally.token"]);
+        sally = await upgrades.deployProxy(NFTFactory, ["Sally-O-Lantern", "SALLY", "sally.token", sallyPrice]);
         sallyV2 = await upgrades.upgradeProxy(sally.address, NFTFactoryV2);
+        await jackV2.grantRole(minter, owner.address)
+        await sallyV2.grantRole(minter, alice.address)
     })
 
     describe("Init", async() => {
@@ -34,70 +36,69 @@ describe("NFTFactory", () => {
             expect(jack).to.be.ok
             expect(sally).to.be.ok
         })
+
+        it("should display minter role", async() => {
+            expect(await jack.hasRole(minter, owner.address))
+                .to.be.true
+            
+            expect(await sally.hasRole(minter, alice.address))
+                .to.be.true
+        })
     })
 
     describe("Mint", async() => {
-        it("should mint from V2 contract and reflect balance in base contract", async() => {
-            //console.log(nftFactory)
-            let jackData = "jack"
-            await jackV2.mint(owner.address, 0, 5, toUtf8Bytes(jackData))
-            await sallyV2.mint(alice.address, 0, 6, toUtf8Bytes(jackData))
+        it("should mint tokens", async() => {
+            await jackV2.mint(alice.address)
+            expect(await jack.balanceOf(alice.address))
+                .to.eq(1)
+        })
 
-            expect(await formatNftBalance(jack, owner.address, 0))
-                .to.eq(5)
-            expect(await formatNftBalance(sally, alice.address, 0))
-                .to.eq(6)
+        it("should return total supply", async() => {
+            await jackV2.mint(alice.address)
+            await jackV2.mint(alice.address)
+            await jackV2.mint(owner.address)
+            expect(await jack.totalSupply())
+                .to.eq(3)
+        })
+
+        it("should enumerate tokenIds", async() => {
+            await jackV2.mint(alice.address)
+            await jackV2.mint(alice.address)
+            await jackV2.mint(owner.address)
+            let res = await jack.tokenOfOwnerByIndex(owner.address, 0)
+            expect(res).to.eq(2)
+
+        })
+
+        it("should revert mint from non-minter", async() => {
+            await expect(jackV2.connect(alice).mint(owner.address))
+                .to.be.reverted
         })
     })
 
     describe("Pause", async() => {
-        let pauser;
-
         beforeEach(async() => {
-            pauser = await jackV2.PAUSER_ROLE()
-            await jackV2.grantRole(pauser, owner.address)
-            await jackV2.mint(owner.address, 0, 5, toUtf8Bytes(""))
-            await sallyV2.mint(alice.address, 0, 6, toUtf8Bytes(""))
-        })
-        it("should grant pauser role in V2 and reflect in base state", async() => {
-            await jack.hasRole(pauser, owner.address)
-        })
-
-        it("should pause and unpause nft tx", async() => {
-            // check transfers okay
-            await jackV2.safeTransferFrom(owner.address, alice.address, 0, 1, toUtf8Bytes(""))
-            
-            await jackV2.pause()
-            await expect(jackV2.safeTransferFrom(owner.address, alice.address, 0, 1, toUtf8Bytes("")))
-                .to.be.revertedWith("ERC1155Pausable: token transfer while paused")
-
-            await jackV2.unpause()
-            expect(await jackV2.safeTransferFrom(owner.address, alice.address, 0, 1, toUtf8Bytes("")))
-                .to.be.ok
-        })
-    })
-
-    describe("Access Control", async() => {
-        let minter
-        let pauser
-
-        beforeEach(async() => {
-            minter = await jackV2.MINTER_ROLE()
-            pauser = await jackV2.PAUSER_ROLE()
-            await jackV2.grantRole(minter, alice.address)
             await jackV2.grantRole(pauser, alice.address)
         })
 
-        it("should revoke role in V2 and reflect in base", async() => {
-            await jackV2.revokeRole(minter, alice.address)
-            await expect(jack.connect(alice).mint(owner.address, 1, 1, toUtf8Bytes("")))
-                .to.be.revertedWith("NFTFactory: must have minter role to mint")
+        it("should assign pause role", async() => {
+            expect(await jack.hasRole(pauser, alice.address))
+                .to.be.true
         })
 
-        it("should renounce role in base and extend to V2", async() => {
-            await jack.connect(alice).renounceRole(pauser, alice.address)
-            await expect(jackV2.connect(alice).pause())
-                .to.be.revertedWith("NFTFactory: must have pauser role to pause")
+        it("should pause", async() => {
+            await jackV2.connect(alice).pause()
+            await expect(jack.mint(owner.address))
+                .to.be.reverted
+        })
+
+        it("should pause and unpause", async() => {
+            await jackV2.connect(alice).pause()
+            await expect(jack.mint(owner.address))
+                .to.be.reverted
+            await jackV2.connect(alice).unpause()
+            expect(await jack.mint(owner.address))
+                .to.be.ok
         })
     })
 })
