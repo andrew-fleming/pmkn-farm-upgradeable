2ac498f14056d410f66dc562822f601575a20949
Andrew <fleming-andrew@protonmail.com>
2021-07-08 01:03:21 -0400

    Started deployment scripts, need to transfer roles

---
+++ scripts/deployFarm.ts
@@ -0,0 +1,41 @@
+import { ethers, upgrades } from "hardhat";
+import { lottoConfig, farmConfig } from "./config";
+
+async function main() {
+    const [deployer] = await ethers.getSigners()
+    console.log(`Deploying contracts with ${deployer.address}`);
+
+    const PmknToken = await ethers.getContractFactory("PmknToken");
+    const pmknToken = await upgrades.deployProxy(PmknToken, [deployer.address, "PmknToken", "PMKN"]);
+    await pmknToken.deployed();
+    console.log(`PmknToken address: ${pmknToken.address}`)
+
+    const NFTFactory = await ethers.getContractFactory("NFTFactory");
+    const nftFactory = await upgrades.deployProxy(NFTFactory, ["Jack-O-Lanter", "JACK", "jack.token", 1])
+    await nftFactory.deployed();
+    console.log(`NFTFactory address: ${nftFactory.address}`)
+
+    const Lottery = await ethers.getContractFactory("Lottery");
+    const lottery = await Lottery.deploy(nftFactory.address, pmknToken.address, ...lottoConfig);
+    console.log(`Lottery address: ${lottery.address}`)
+
+    const PmknFarm = await ethers.getContractFactory("PmknFarm");
+    const pmknFarm = await upgrades.deployProxy(PmknFarm, 
+        [
+            deployer.address, 
+            ...farmConfig, 
+            pmknToken.address, 
+            nftFactory.address, 
+            lottery.address
+        ]
+    );
+    await pmknFarm.deployed();
+    console.log(`PmknFarm address: ${pmknFarm.address}`)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.log(error)
+        process.exit(1)
+    })
